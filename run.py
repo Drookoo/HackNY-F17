4eb4038f1ff8b9561220483474a305a923534e59
Drew Ku <andrewku123@gmail.com>
2017-10-14 19:56:59 -0400

    Works

--- run.py
+++ run.py
@@ -1,24 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 from twilio.rest import Client
 from twilio.twiml.messaging_response import MessagingResponse
+from twilio import twiml
 import requests
+
 
 app = Flask(__name__)
 
 client = Client('AC2f71f255524180a45b9acf3a55766db6', '69aa8e008a80ce5248f72429b5756fa5')
 
 @app.route('/', methods=['GET', 'POST'])
-def hello_world():
-    zipcode = []
-    url = 'https://data.cityofnewyork.us/resource/byk8-bdfw.json'
-    dataset = requests.get(url).json()
-    for e in dataset:
-        zipcode.append('"postcode"')
-    print(zipcode)
-
-
+def sms():
+    message_body = request.form['Body']
     resp = MessagingResponse()
-    resp.message("Hello, Angel")
+    resp.message('Hello, your zip is {}'.format(message_body))
     return str(resp)
 
 if __name__ == "__main__":
--- search.py
+++ search.py
@@ -1,8 +1,11 @@
 import requests
+
 zipcode = []
+dreamset = {}
 url = 'https://data.cityofnewyork.us/resource/byk8-bdfw.json'
 dataset = requests.get(url).json()
 for e in range(47):
    zipcode.append(dataset[e]['postcode'])
 
-print(zipcode)
+want = input("zipcode: ")
+print(zipcode.count(want))
